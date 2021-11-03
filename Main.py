b65d5de872b1be8715413f0a27a1121bd8845874
Ahnaf Hamid <mhamid161275@bscse.uiu.ac.bd>
2021-11-03 13:17:35 +0600

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,67 @@
+import logging
+
+import requests
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Enter text in Bangla')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Enter text in Bangla')
+
+
+def echo(update, context):
+    response = requests.post('https://www.cbdnlp.xyz/predict', json={'content': update.message.text})
+    cat = list((response.json()).keys())
+    prob = list((response.json()).values())
+    update.message.reply_text(
+        'This comment contains "' + cat[0] + '" cyberbullying with a probability of ' + prob[0] + "%")
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    updater = Updater("2058298945:AAGBhlrUhS75p621dHJVmkSGGDgnVBerK7I", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
